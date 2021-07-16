c1ebb160942ed2f3499898ecd028c3d954ce3a72
youngho-C <cyh6099@gmail.com>
2021-07-16 22:47:17 +0900

    implement comment delete

--- comment/urls.py
+++ comment/urls.py
@@ -8,5 +8,7 @@ from comment import views
 app_name = 'comments'
 
 urlpatterns = [
-    path('create/<int:content_id>', views.create, name = 'create')
+    path('create/<int:content_id>', views.create, name = 'create'),
+    # path('update/<int:comment_id>', views.update, name = 'update'),
+    path('delete/<int:comment_id>', views.delete, name = 'delete'),
 ]
--- comment/views.py
+++ comment/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from comment.forms import CommentCreationForm
+from comment.models import Comments
 
 
 def create(request, content_id):
@@ -18,4 +19,14 @@ def create(request, content_id):
             comment_info.save()
         return redirect('board:content_detail', content_id)
 
+    return redirect('board:content_detail', content_id)
+
+# def update(request, comment_id):
+
+
+def delete(request, comment_id):
+
+    delete_comment = Comments.objects.get(id=comment_id)
+    content_id = delete_comment.content_id
+    delete_comment.delete()
     return redirect('board:content_detail', content_id)
